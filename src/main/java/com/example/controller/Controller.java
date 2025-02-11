55dda9cafe4c7e5125ded1689522a5a9fe755747
Aravinda Reddy Gangalakunta <157139192+Aravinda-Reddy-Gangalakunta@users.noreply.github.com>
2025-02-11 18:20:21 +0000

    Added mapper and reducer and output code

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -1,9 +1,31 @@
 package com.example.controller;
 
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
 
-public class Controller{
-
-    public static void main(String[] args){
-        
+import com.example.WordMapper;
+import com.example.WordReducer;
+public class Controller {
+    public static void main(String[] args) throws IOException{
+        JobConf conf = new JobConf(Controller.class);
+        conf.setJobName("WordCount");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setMapperClass(WordMapper.class);
+        conf.setCombinerClass(WordReducer.class);
+        conf.setReducerClass(WordReducer.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+        FileInputFormat.setInputPaths(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
     }
 }
